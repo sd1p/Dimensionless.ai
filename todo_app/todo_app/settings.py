40ffe8bc01e646b20eed43295f623da34a720bfe
Sudip Mandal <skmxyt@gmail.com>
2024-11-26 21:09:54 +0530

    Updated backend CORS policy

--- todo_app/todo_app/settings.py
+++ todo_app/todo_app/settings.py
@@ -50,10 +50,12 @@ INSTALLED_APPS = [
     
     "drf_yasg",
     "todos",
+    'corsheaders',
 
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -61,6 +63,17 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOW_METHODS = [
+    'GET',
+    'POST',
+    'PUT',
+    'PATCH',
+    'DELETE',
+    'OPTIONS',
 ]
 
 ROOT_URLCONF = "todo_app.urls"
