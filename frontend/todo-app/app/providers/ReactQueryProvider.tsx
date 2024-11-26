b27cbe128d9a79b080cb985c2e74d45efe64dd1a
Sudip Mandal <skmxyt@gmail.com>
2024-11-26 23:25:23 +0530

    update frontend

--- frontend/todo-app/app/providers/ReactQueryProvider.tsx
+++ frontend/todo-app/app/providers/ReactQueryProvider.tsx
@@ -1,4 +1,4 @@
-"use client"; // Important for App Router
+"use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
